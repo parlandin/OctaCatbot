5587aac0fac2b5da2d8a2b0105d1cf0e63f2eea9
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-17 15:31:27 -0300

    feat: add PDF to image conversion in DocumentHandler for enhanced document processing

--- src/application/events/Document.ts
+++ src/application/events/Document.ts
@@ -1,9 +1,14 @@
 //application/pdf
 //application/msword
 
-import { WAMessage, WASocket } from "@whiskeysockets/baileys";
+import {
+  downloadMediaMessage,
+  WAMessage,
+  WASocket,
+} from "@whiskeysockets/baileys";
 import { container } from "tsyringe";
 import { Logger } from "@infrastructure/Logger.js";
+import { fromBuffer } from "pdf2pic";
 
 import { reactMessage, replyMessage } from "@utils/quoteMessage";
 
@@ -15,6 +20,38 @@ export async function execute(socket: WASocket, message: WAMessage) {
   try {
     if (!message.key.remoteJid) return;
     if (!message.message?.documentMessage) return;
+
+    await reactMessage(socket, message, "⏳");
+
+    if (message.message.documentMessage.mimetype == "application/pdf") {
+      const media = await downloadMediaMessage(message, "buffer", {});
+
+      if (!media) {
+        logger.warn("Mídia não encontrada!");
+        return;
+      }
+
+      const images = await fromBuffer(media, {
+        preserveAspectRatio: true,
+        quality: 90,
+        density: 90,
+        compression: "JPEG",
+        format: "png",
+      }).bulk(-1, {
+        responseType: "buffer",
+      });
+
+      if (images.length <= 0) return;
+
+      for (const image of images) {
+        if (!image.buffer) continue;
+        await replyMessage(socket, message, {
+          image: image.buffer,
+        });
+      }
+
+      return;
+    }
 
     await reactMessage(socket, message, "⏳");
 
